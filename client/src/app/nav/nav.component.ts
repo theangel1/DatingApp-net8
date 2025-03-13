1a15bd7f38e9ec8fc2ae00ea973d3aacebe6d6f2
theAngel1 <angel.pinilla@outlook.es>
2025-03-13 17:40:15 -0300

    usando componente angular de bootstrap dropdown

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
@@ -15,7 +16,7 @@ export class NavComponent {
   loggedIn = false
   model: any = {};
 
-  login() {    
+  login() {
     //lo siguiente retornará un observable.
     this.accountService.login(this.model).subscribe({
       next: response => {
@@ -26,7 +27,7 @@ export class NavComponent {
     })
   }
 
-  logout(){
+  logout() {
     this.loggedIn = false
   }
 }
