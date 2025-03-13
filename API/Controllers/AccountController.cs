5923eb42f0b01f563302ad8d964528e4a742a5d3
theAngel1 <angel.pinilla@outlook.es>
2025-03-13 14:48:16 -0300

    Se agrega servicio de token y tokenHandler para login.

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -31,6 +31,24 @@ public class AccountController(DataContext context) : BaseApiController
 
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
+
+        if(user == null) return Unauthorized("Username inválido");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("password inválido");
+        }
+        return user;
+    }
+
     private async Task<bool> UserExists(string username)
     {
         return await context.Users.AnyAsync( x => x.UserName.ToLower() == username.ToLower());
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services;
+
+public class TokenService(IConfiguration config) : ITokenService
+{
+    public string CreateToken(AppUser user)
+    {
+        var tokenKey = config["TokenKey"] ?? throw new Exception("No se puede acceder al token desde appsettings");
+
+        if(tokenKey.Length < 64) throw new Exception("Token debe ser más largo");
+        var key  = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey));
+        
+        var claims = new List<Claim>
+        {
+            new (ClaimTypes.NameIdentifier, user.UserName)
+        };
+
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature); //por eso el lenght de 64
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials = creds
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
