06e50ca90ba8553cb2367cfc03c7425e07f57cf6
theAngel1 <angel.pinilla@outlook.es>
2025-03-13 11:14:50 -0300

    userEntity updated. Refactor baseapicontroller.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,11 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-
-public class UsersController(DataContext context) : ControllerBase
+public class UsersController(DataContext context) : BaseApiController
 {
     
 
