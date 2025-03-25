394997432057721d0950491afea113581e71d944
theAngel1 <angel.pinilla@outlook.es>
2025-03-25 14:30:41 -0300

    se agrega nav link router

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,25 +2,26 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService = inject(AccountService)
-  
+
   model: any = {};
 
   login() {
     //lo siguiente retornará un observable.
     this.accountService.login(this.model).subscribe({
       next: response => {
-        console.log(response)        
+        console.log(response)
       },
       error: error => console.log(error)
     })
