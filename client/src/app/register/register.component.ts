5773e92d9af6339e02b569959b76be565a730404
theAngel1 <angel.pinilla@outlook.es>
2025-03-25 12:09:45 -0300

    comunicacion entre hijo y padre (register a home)

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, Input } from '@angular/core';
+import { Component,  input,  output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -10,13 +10,14 @@ import { FormsModule } from '@angular/forms';
 })
 export class RegisterComponent {
   usersFromHomeComponent = input.required<any>()
+  cancelRegister = output<boolean>()
   model: any = {}
 
   register() {
     console.log(this.model)
   }
   cancel() {
-    console.log("cancelled")
+    this.cancelRegister.emit(false)
   }
 
 }
