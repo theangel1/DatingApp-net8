991e76fd4d8384c80fe3c02e4d1f61e1db3139c5
theAngel1 <angel.pinilla@outlook.es>
2025-03-27 17:24:26 -0300

    se agrega server error page

--- client/src/app/errors/server-error/server-error.component.ts
+++ client/src/app/errors/server-error/server-error.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-server-error',
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './server-error.component.css'
 })
 export class ServerErrorComponent {
+//buen momento para crear un constructor en esta clase porq es la unica forma de acceder a navigationExtras...
+error:any
 
+constructor(private router: Router){
+  const navigation = this.router.getCurrentNavigation()
+  //todo deber opcional ya que puede retornar undefined
+  this.error = navigation?.extras?.state?.['error'];
+}
 }
