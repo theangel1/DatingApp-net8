ac0de966d6df7a8e6167f45971d4b97d9eafc6fb
theAngel1 <angel.pinilla@outlook.es>
2025-04-01 16:08:42 -0300

    se agrega migracion updatedUserEntityPhotos

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities;
 
+//esto por un tema de convención. Por experiencia, a veces estás tablas se creaban automaticamente como "photoes"
+[Table("Photos")]
 public class Photo
 {
     public int Id { get; set; }
     public required string Url { get; set; }
     public bool IsMain { get; set; }
     public string? PublicId { get; set; }
+
+    //navigation properties
+
+    public int AppUserId { get; set; }
+
+    //para que fotos no tenga un appuser nulo, ponemos null!
+    public AppUser AppUser { get; set; } = null!;
 }
