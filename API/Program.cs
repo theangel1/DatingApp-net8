9d1b89d9c97dc87de0e3ae60f5a95e14143ec3d9
theAngel1 <angel.pinilla@outlook.es>
2025-04-01 18:13:22 -0300

    fix propiedad knownAs y updated a gitignore

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using API.Data;
 using API.Extensions;
 using API.Middleware;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,5 +20,22 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//aplicando migraciones y el seed. Es fuera del dependency injection, asi que... ojo
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+//try catch porq no estamos usando middleware exceptions
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "ocurrió un error durante la migracion");
+}
 
 app.Run();
