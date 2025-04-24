a7fb75cf8fa664f60f86d7555ce9a90128c9dae8
theAngel1 <angel.pinilla@outlook.es>
2025-04-24 09:33:02 -0400

    Retornando lista de miembros

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
-import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-home',
@@ -9,29 +9,17 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
-  http = inject(HttpClient);
-  registerMode = false
-  users: any;
+export class HomeComponent {
 
-  ngOnInit(): void {
-    this.getUsers()
-  }
+  registerMode = false
 
   registerToggle() {
     this.registerMode = !this.registerMode
   }
 
-  cancelRegisterMode (event: boolean){
-this.registerMode = event
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event
   }
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log("request completado! ya no estamos suscritos al observable!")
 
-    })
-  }
 
 }
