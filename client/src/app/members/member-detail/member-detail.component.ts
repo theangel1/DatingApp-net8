b4b68dd68eb7fb19b8f0d82ae853f4afdcc03b8e
theAngel1 <angel.pinilla@outlook.es>
2025-04-24 11:29:26 -0400

    se agrega gallery photo

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,29 +3,36 @@ import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [TabsModule],
+  imports: [TabsModule, GalleryModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
-export class MemberDetailComponent implements OnInit{
-  
+export class MemberDetailComponent implements OnInit {
+
   private memberService = inject(MembersService)
   private route = inject(ActivatedRoute)
   member?: Member
+  images: GalleryItem[] = []
 
   ngOnInit(): void {
     this.loadMember()
   }
-  loadMember(){
+  loadMember() {
     const username = this.route.snapshot.paramMap.get('username')
-    if(!username) return
+    if (!username) return
 
     this.memberService.getMember(username).subscribe({
-      next: member => this.member = member
+      next: member => {
+        this.member = member
+        member.photos.map(p => {
+          this.images.push(new ImageItem({ src: p.url, thumb: p.url }))
+        })
+      }
     })
   }
 }
