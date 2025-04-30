bddbf2e019c73278036a7b38bed99ba99232010a
theAngel1 <angel.pinilla@outlook.es>
2025-04-30 16:16:10 -0400

    agregando servicio de fotos

---
+++ API/Interfaces/IPhotoService.cs
@@ -0,0 +1,13 @@
+using System;
+using CloudinaryDotNet.Actions;
+
+namespace API.Interfaces;
+
+public interface IPhotoService
+{
+    Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+    Task<DeletionResult> DeletePhotoAsync(string publicId);
+
+}
+
+
