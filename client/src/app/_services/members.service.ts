fac5a181e84e02077101f299264844bb0acee0cc
theAngel1 <angel.pinilla@outlook.es>
2025-08-10 18:31:19 -0400

    setting the main photo in the client

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -3,6 +3,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
 import { of, tap } from 'rxjs';
+import { Photo } from '../_models/Photo';
 
 
 @Injectable({
@@ -21,19 +22,32 @@ export class MembersService {
 
   getMember(username: string) {
     const member = this.members().find(x => x.userName === username)
-    if(member != undefined)  return of(member)
+    if (member != undefined) return of(member)
 
     return this.http.get<Member>(this.baseUrl + 'users/' + username)
   }
 
-  updateMember(member: Member){
+  updateMember(member: Member) {
     return this.http.put(this.baseUrl + 'users', member).pipe(
       tap(() => {
-        this.members.update(members => members.map(m => m.userName === member.userName 
-          ? member: m))
+        this.members.update(members => members.map(m => m.userName === member.userName
+          ? member : m))
       })
     )
 
+  }
+
+  setMainPhoto(photo: Photo) {
+    return this.http.put(this.baseUrl + 'users/set-main-photo/' + photo.id, {}).pipe(
+      tap(() => {
+        this.members.update(members=> members.map(m => {
+          if(m.photos.includes(photo)){
+            m.photoUrl = photo.url
+          }
+          return m
+        }))
+      })
+    )
   }
 
 
