1d84902b3b1b8503d86f0923a7c8c04d54cd605e
theAngel1 <angel.pinilla@outlook.es>
2025-08-12 20:39:59 -0400

    adding sorting functionality

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -21,6 +21,9 @@ public class UserParams
 
     public int MinAge { get; set; } = 18;
     public int MaxAge { get; set; } = 100;
+
+    public string OrderBy { get; set; } = "lastActive";
+    
     
 
 }
