0528b0c71a37f0b6a1b34cf1b88d90fd34ebb0c2
theAngel1 <angel.pinilla@outlook.es>
2025-08-14 10:45:26 -0400

    adding the toggle functionality

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -16,4 +16,17 @@ export class MemberCardComponent {
   //computed signal
   hasLiked = computed(()=> this.likeService.likeIds().includes(this.member().id))
 
+  toggleLike(){
+    this.likeService.toggleLike(this.member().id).subscribe({
+      next: () => {
+        if(this.hasLiked()){
+          this.likeService.likeIds.update(ids => ids.filter(x => x !== this.member().id))          
+        }
+        else{
+          this.likeService.likeIds.update(ids =>[...ids, this.member().id])
+        }
+      }
+    })
+  }
+
 }
