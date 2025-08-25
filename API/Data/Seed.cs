feb60d97ba0a34b6dfb9e8fd855b506e250f5bff
theAngel1 <angel.pinilla@outlook.es>
2025-08-25 09:28:31 -0400

    updating the seed method

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -3,16 +3,17 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
 public class Seed
 {
-    public static async Task SeedUsers(DataContext context)
+    public static async Task SeedUsers(UserManager<AppUser> userManager)
     {
         //si hay usuarios, entonces no hago nada
-        if (await context.Users.AnyAsync()) return ;
+        if (await userManager.Users.AnyAsync()) return ;
 
         var userData = await File.ReadAllTextAsync("Data/UserDataSeed.json");
 
@@ -24,13 +25,9 @@ public class Seed
         
         foreach (var user in users)
         {
-            using var hmac = new HMACSHA512();           
-           
-            context.Users.Add(user);
+            await userManager.CreateAsync(user, "Pa$$w0rd");
             
         }
-
-        await context.SaveChangesAsync();
 
     }
 
