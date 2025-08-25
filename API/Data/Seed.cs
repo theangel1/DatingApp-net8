7dacf2fd1a2dcc8a66ae88bcbda9e12a1d38af1c
theAngel1 <angel.pinilla@outlook.es>
2025-08-25 09:39:03 -0400

    updating the account controller

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -25,6 +25,7 @@ public class Seed
         
         foreach (var user in users)
         {
+            user.UserName = user.UserName!.ToLower(),
             await userManager.CreateAsync(user, "Pa$$w0rd");
             
         }
