245c7252891adafbb7efc7ed2985c1b390a29683
theAngel1 <angel.pinilla@outlook.es>
2025-08-26 16:41:39 -0400

    client side signalR

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -4,6 +4,7 @@ import { User } from '../_models/user';
 import { map } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { LikesService } from './likes.service';
+import { PresenceService } from './presence.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +13,14 @@ export class AccountService {
 
   private http = inject(HttpClient)
   private likeService = inject(LikesService)
+  private presenceService = inject(PresenceService);
   baseUrl = environment.apiUrl
   currentUser = signal<User | null>(null)
   roles = computed(() => {
     const user = this.currentUser();
-    if(user && user.token){
+    if (user && user.token) {
       const role = JSON.parse(atob(user.token.split('.')[1])).role
-    return Array.isArray(role) ? role: [role];
+      return Array.isArray(role) ? role : [role];
     }
 
     return [];
@@ -51,12 +53,14 @@ export class AccountService {
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUser.set(user);
     this.likeService.getLikeIds();
+    this.presenceService.createHubConnection(user);
   }
 
 
   logout() {
     localStorage.removeItem('user')
     this.currentUser.set(null)
+    this.presenceService.stopHubConnection();
   }
 
 }
