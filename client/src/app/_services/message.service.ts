bd628d3fd769ecc047831e9fe0df9272c371bc59
theAngel1 <angel.pinilla@outlook.es>
2025-08-26 18:04:11 -0400

    adding the hub connectio to the message service

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -4,6 +4,8 @@ import { HttpClient } from '@angular/common/http';
 import { PaginatedResult } from '../_models/pagination';
 import { Message } from '../_models/message';
 import { setPaginatedResponse, setPaginationHeaders } from './paginationHelper';
+import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@microsoft/signalr';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +13,37 @@ import { setPaginatedResponse, setPaginationHeaders } from './paginationHelper';
 export class MessageService {
 
   baseUrl = environment.apiUrl;
+  hubUrl = environment.hubsUrl;
   private http = inject(HttpClient);
+  private hubConnection?: HubConnection;
   paginatedResult = signal<PaginatedResult<Message[]> | null>(null);
+  messageThread = signal<Message[]>([]);
+
+  createHubConnection(user: User, otherUsername: string){
+    this.hubConnection = new HubConnectionBuilder()
+    .withUrl(this.hubUrl + 'message?user=' + otherUsername,{
+      accessTokenFactory:( ) => user.token
+    })
+    .withAutomaticReconnect()
+    .build()
+
+    this.hubConnection.start().catch(error => console.log(error))
+
+    //y cuando recibimos el evento de message...
+    this.hubConnection.on('ReceiveMessageThread', messages => {
+      this.messageThread.set(messages)
+    })
+  }
+
+  stopHubConnection(){
+    
+    if(this.hubConnection?.state ===HubConnectionState.Connected){
+      this.hubConnection.stop().catch(error => console.log(error))
+    }
+  }
+
+
+
 
   getMessages(pageNumber: number, pageSize: number, container: string){
     let params = setPaginationHeaders(pageNumber, pageSize);
