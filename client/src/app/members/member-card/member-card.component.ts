edd191b17c3b9e8597751d2d94dd860f7cf1ee34
theAngel1 <angel.pinilla@outlook.es>
2025-08-26 17:40:27 -0400

    displaying online presence

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject, input } from '@angular/core';
 import { Member } from '../../_models/member';
 import { RouterLink } from '@angular/router';
 import { LikesService } from '../../_services/likes.service';
+import { PresenceService } from '../../_services/presence.service';
 
 @Component({
   selector: 'app-member-card',
@@ -12,9 +13,11 @@ import { LikesService } from '../../_services/likes.service';
 })
 export class MemberCardComponent {
   private likeService = inject(LikesService);
+  private presenceService = inject(PresenceService);
   member = input.required<Member>();
   //computed signal
   hasLiked = computed(()=> this.likeService.likeIds().includes(this.member().id))
+  isOnline = computed(() => this.presenceService.onlineUsers().includes(this.member().userName))
 
   toggleLike(){
     this.likeService.toggleLike(this.member().id).subscribe({
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject, OnInit, ViewChild } from '@angular/core';
-import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { TabDirective, TabsetComponent, TabsModule } from 'ngx-bootstrap/tabs';
@@ -9,6 +8,7 @@ import { DatePipe } from '@angular/common';
 import { MemberMessagesComponent } from "../member-messages/member-messages.component";
 import { Message } from '../../_models/message';
 import { MessageService } from '../../_services/message.service';
+import { PresenceService } from '../../_services/presence.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -19,20 +19,21 @@ import { MessageService } from '../../_services/message.service';
 })
 export class MemberDetailComponent implements OnInit {
 
-  @ViewChild('memberTabs', {static: true}) memberTabs?: TabsetComponent;
+  @ViewChild('memberTabs', { static: true }) memberTabs?: TabsetComponent;
   private messageService = inject(MessageService);
-  private memberService = inject(MembersService)
+  presenceService = inject(PresenceService)
   private route = inject(ActivatedRoute)
+
   member: Member = {} as Member;
   images: GalleryItem[] = []
-  activeTab? : TabDirective;
-  messages : Message[] = [];
+  activeTab?: TabDirective;
+  messages: Message[] = [];
 
   ngOnInit(): void {
     this.route.data.subscribe({
       next: data => {
-        this.member= data['member'];
-       this.member &&  this.member.photos.map(p => {
+        this.member = data['member'];
+        this.member && this.member.photos.map(p => {
           this.images.push(new ImageItem({ src: p.url, thumb: p.url }))
         })
       }
@@ -45,10 +46,10 @@ export class MemberDetailComponent implements OnInit {
     })
   }
 
-  selectTab(heading: string){
-    if(this.memberTabs){
+  selectTab(heading: string) {
+    if (this.memberTabs) {
       const messageTab = this.memberTabs.tabs.find(x => x.heading === heading)
-      if(messageTab){
+      if (messageTab) {
         messageTab.active = true;
       }
     }
@@ -57,31 +58,31 @@ export class MemberDetailComponent implements OnInit {
   onTabActivated(data: TabDirective) {
     this.activeTab = data;
 
-    if(this.activeTab.heading === 'Messages' && this.messages.length === 0 && this.member){
-       this.messageService.getMessageThread(this.member.userName).subscribe({
-      next: messages => this.messages = messages
-    })
+    if (this.activeTab.heading === 'Messages' && this.messages.length === 0 && this.member) {
+      this.messageService.getMessageThread(this.member.userName).subscribe({
+        next: messages => this.messages = messages
+      })
     }
   }
 
-onUpdateMessages(event: Message){
-  this.messages.push(event);
-}
+  onUpdateMessages(event: Message) {
+    this.messages.push(event);
+  }
 
 
   //no usaremos el siguiente metodo debido a que el resolver se encarga de hacer el load del member
-/*
-  loadMember() {
-    const username = this.route.snapshot.paramMap.get('username')
-    if (!username) return
-
-    this.memberService.getMember(username).subscribe({
-      next: member => {
-        this.member = member
-        member.photos.map(p => {
-          this.images.push(new ImageItem({ src: p.url, thumb: p.url }))
-        })
-      }
-    })
-  }*/
+  /*
+    loadMember() {
+      const username = this.route.snapshot.paramMap.get('username')
+      if (!username) return
+  
+      this.memberService.getMember(username).subscribe({
+        next: member => {
+          this.member = member
+          member.photos.map(p => {
+            this.images.push(new ImageItem({ src: p.url, thumb: p.url }))
+          })
+        }
+      })
+    }*/
 }
