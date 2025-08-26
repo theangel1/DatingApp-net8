981c3d828289013a4a1c08a1f5348d1ea7bbc0cf
theAngel1 <angel.pinilla@outlook.es>
2025-08-26 16:24:06 -0400

    adding a presence hub

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -27,6 +27,7 @@ public static class ApplicationServiceExtensions
         services.AddScoped<IMessageRepository, MessageRepository>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+        services.AddSignalR();
         
         return services;
 
