028e86790a6d7fbc0c05df5f3a3a8321811d9401
theAngel1 <angel.pinilla@outlook.es>
2025-08-27 10:59:27 -0400

    dealing with utc date formats

--- API/Helpers/AutoMapperProfile.cs
+++ API/Helpers/AutoMapperProfile.cs
@@ -25,6 +25,11 @@ public class AutoMapperProfile : Profile
         o => o.MapFrom(s => s.Sender.Photos.FirstOrDefault(x => x.IsMain)!.Url))
         .ForMember(d => d.RecipientPhotoUrl,
         o => o.MapFrom(s => s.Recipient.Photos.FirstOrDefault(x => x.IsMain)!.Url));
+
+        //utc dates
+        CreateMap<DateTime, DateTime>().ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
+        CreateMap<DateTime?, DateTime?>().ConvertUsing(d => d.HasValue
+        ? DateTime.SpecifyKind(d.Value, DateTimeKind.Utc) : null);
     }
 
 }
