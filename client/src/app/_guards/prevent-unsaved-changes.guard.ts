8e3df29f3a7725763229dd4d99ceefd95a844c23
theAngel1 <angel.pinilla@outlook.es>
2025-08-28 16:08:29 -0400

    adding a confirmation service to the angular app

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,10 +1,14 @@
 import { CanDeactivateFn } from '@angular/router';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { inject } from '@angular/core';
+import { ConfirmService } from '../_services/confirm.service';
 
 //este guard solo nos ayuda con rutas de angular, no con cosas fuera como en el browser
 export const preventUnsavedChangesGuard: CanDeactivateFn<MemberEditComponent> = (component) => {
+  const confirmService = inject(ConfirmService);
+
   if(component.editForm?.dirty){
-    return confirm('are you sure you want to continue? any unsaved changes will be lost')
+    return confirmService.confirm() ?? false;
   }
   return true
 };
